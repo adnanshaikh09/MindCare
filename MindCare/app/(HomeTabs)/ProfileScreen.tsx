5e5b4fd0d562532b372aebf7c6e0dc151953aded
Adnan Shaikh <shaikhadnan.2016@gmail.com>
2024-10-20 12:21:01 -0400

    #21  Create Profile Screen

---
+++ MindCare/app/(HomeTabs)/ProfileScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ProfileScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the Profile!</Text>
+    </View>
+  );
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
