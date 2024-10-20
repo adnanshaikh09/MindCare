032f0a844761e2c823e3fc2c121a6030a74a685a
Meet Shah <meet30012002@gmail.com>
2024-10-20 16:13:54 -0400

    Update Profile pages

--- MindCare/components/AppointmentsSection.tsx
+++ MindCare/components/AppointmentsSection.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import AppointmentCard from './AppointmentCard';
 
-const AppointmentsSection = () => {
+
+const AppointmentsSection: React.FC = () => {
   return (
-    <div>AppointmentsSection</div>
-  )
-}
+    <View style={styles.container}>
+      <Text style={styles.title}>Future Appointments:</Text>
+      <AppointmentCard
+        doctorName="Dr. Selkon Kane"
+        specialization="Psychiatrist"
+        date="16-Nov-2020 | 5:00 PM"
+      />
+      <Text style={styles.title}>Past Appointments:</Text>
+      <AppointmentCard
+        doctorName="Dr. Brain Wofe"
+        specialization="Psychologist"
+        date="10-Nov-2020 | 4:00 PM"
+      />
+            <AppointmentCard
+        doctorName="Dr. Brain Wofe"
+        specialization="Psychologist"
+        date="10-Nov-2020 | 4:00 PM"
+      />
+            <AppointmentCard
+        doctorName="Dr. Brain Wofe"
+        specialization="Psychologist"
+        date="10-Nov-2020 | 4:00 PM"
+      />
+            <AppointmentCard
+        doctorName="Dr. Brain Wofe"
+        specialization="Psychologist"
+        date="10-Nov-2020 | 4:00 PM"
+      />
+    </View>
+  );
+};
 
-export default AppointmentsSection
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#4caf50',
+    marginBottom: 10,
+  },
+});
+
+export default AppointmentsSection;
