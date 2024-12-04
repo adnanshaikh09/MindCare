681c69519b8da86959d16bb75b0d5d6ab9587a3f
Adnan Shaikh <shaikhadnan.2016@gmail.com>
2024-12-03 20:18:22 -0500

    Update ChatScreen

--- MindCare/app/ChatScreen.tsx
+++ MindCare/app/ChatScreen.tsx
@@ -71,7 +71,7 @@ const ChatScreenMain = () => {
   
         setMessages((prevMessages) => [
           ...prevMessages,
-          ...fetchedMessages.reverse(), // Reverse to show oldest messages first
+          ...fetchedMessages, // Reverse to show oldest messages first
         ]);
         setNextPage(data.next ? page + 1 : null); // Update nextPage if there's more data
       } else if (response.status === 403) {
